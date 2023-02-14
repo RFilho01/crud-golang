be281205e6d011998fd6669b301234f7d502fde8
RFilho01 <robertocorreiafilho0100@outlook.com>
2023-02-13 21:37:52 -0300

    feat: adding logs and new features

---
+++ src/controller/create_user.go
@@ -0,0 +1,42 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/RFilho01/crud-golang/src/configuration/logger"
+	"github.com/RFilho01/crud-golang/src/configuration/validation"
+	"github.com/RFilho01/crud-golang/src/controller/model/request"
+	"github.com/RFilho01/crud-golang/src/controller/response"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller",
+		zap.String("journey", "createUser"),
+	)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "createUser"),
+		)
+		errRest := validation.ValidateUserError(err)
+
+		c.JSON(errRest.Code, errRest)
+		return
+	}
+
+	response := response.UserResponse{
+		ID:    "test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+
+	logger.Info("User created successfully",
+		zap.String("journey", "createUser"),
+	)
+
+	c.JSON(http.StatusOK, response)
+}
